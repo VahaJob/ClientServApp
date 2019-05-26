747c43f84a3ea10a096bf88405957887f556cbbd
VahaJob <01.unbroken@gmail.com>
2019-05-26 19:51:26 +0300

    Added ClientSeeverApp

---
+++ ClietnTCP/Program.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace ClietnTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Input message : ");
+
+            var message = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(message);
+
+            tcpSocket.Connect(tcpEndPoint);
+
+            tcpSocket.Send(data);
+
+          //  var listener = tcpSocket.Accept();
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+
+            do
+            {
+               size = tcpSocket.Receive(buffer);
+               answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            } while (tcpSocket.Available > 0);
+
+            Console.WriteLine(answer);
+
+            tcpSocket.Shutdown(SocketShutdown.Both);
+
+            tcpSocket.Close();
+            Console.ReadLine();
+        
+
+        }
+
+       
+    }
+}
